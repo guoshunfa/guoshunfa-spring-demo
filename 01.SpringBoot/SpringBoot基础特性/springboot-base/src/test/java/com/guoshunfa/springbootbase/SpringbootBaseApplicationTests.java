6653c922039773d8e7905a0d2e3bca5d1a0ac08e
guoshunfa <13301203454@163.com>
2023-07-10 16:24:53 +0800

    添加springboot初始化项目

---
+++ 01.SpringBoot/SpringBoot基础特性/springboot-base/src/test/java/com/guoshunfa/springbootbase/SpringbootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoshunfa.springbootbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
