3c312951e799c4ad39207dfd5065d0918cc99adc
guoshunfa <13301203454@163.com>
2023-07-18 11:06:44 +0800

    添加SpringBoot3集成Quartz详细版项目，内容还未填充

---
+++ 01.SpringBoot/SpringBoot集成组件库/springboot-3-quartz-detailed-demo/src/main/java/com/guoshunfa/springboot3quartzdetaileddemo/Springboot3QuartzDetailedDemoApplication.java
@@ -0,0 +1,13 @@
+package com.guoshunfa.springboot3quartzdetaileddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3QuartzDetailedDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3QuartzDetailedDemoApplication.class, args);
+	}
+
+}
